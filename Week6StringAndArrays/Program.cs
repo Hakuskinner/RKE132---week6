dfc2948f926ea0a56533e77a57ee7bcf0f4a63e1
Maiko <maiko.magi@gmail.com>
2022-12-30 14:50:28 +0200

    first commit

---
+++ Week6StringAndArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿string hello = "we are the champions! ";
+int stringLenght = hello.Length;
+hello = hello.Trim();
+
+char firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+Console.WriteLine(hello);
+
+//int wordcounter = 1;
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordcounter++;
+//    }
+//    }
+//Console.WriteLine($"word count in {hello}: {wordcounter} words.");
